e6eeb269537572b67b8aaf1c62b6bc777921a436
Andrew <drew.khoo92@gmail.com>
2019-10-24 11:00:09 +0100

    homepage added

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+require './lib/grocery_list'
+
+class ListManager < Sinatra::Base
+  enable :sessions, :method_override
+
+  get '/list' do
+    @items = Grocery_List.all?
+    erb :'homepage'
+  end
+
+  post '/list' do
+    Grocery_List.add(item: params['item'], category: params['category']).to_s
+    redirect '/list'
+  end
+end
