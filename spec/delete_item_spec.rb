1e7d6a61f2027a55d084632b8b915d2092052032
Eoin <Gunnery@BGL-MAC-020019.bgl.financial.local>
2019-10-25 11:04:59 +0100

    reset test and code cleanup

--- spec/delete_item_spec.rb
+++ spec/delete_item_spec.rb
@@ -5,14 +5,9 @@ describe Grocery_List do
   describe '.remove' do
     it 'removes an item' do
       connection = PG.connect(dbname: 'grocery_app_test')
+      Grocery_List.remove(item:'onion', category: 'vegetables')
       result = connection.exec("SELECT * FROM grocery_list ORDER BY ID ASC")
-
-      puts result[0]['item']
-
-
-      grocery_list = Grocery_List.remove(item:'onion', category: 'vegetables')
-      result2 = connection.exec("SELECT * FROM grocery_list ORDER BY ID ASC")
-      expect(result2[0]['item']).to eq 'aubergine'
+      expect(result[0]['item']).to eq 'aubergine'
     end
   end
 end
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,9 +3,5 @@ require 'pg'
 p "Setting up test database..."
 
 def setup_test_database
-
   connection = PG.connect(dbname: 'grocery_app_test')
-
-#  connection.exec("TRUNCATE grocery_list;")
-
 end
