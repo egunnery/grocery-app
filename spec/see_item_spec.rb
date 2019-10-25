e7e58ca40227ed59a123281137fa1b04d60fd6bd
Eoin <Gunnery@BGL-MAC-020019.bgl.financial.local>
2019-10-25 11:15:25 +0100

    further cleanup

--- spec/see_item_spec.rb
+++ spec/see_item_spec.rb
@@ -4,6 +4,7 @@ require 'pg'
 describe Grocery_List do
   describe '.all?' do
     it 'returns a list of items' do
+      connection = PG.connect(dbname: 'grocery_app_test')
 
       Grocery_List.add(item: "carrots", category: "vegetables")
       Grocery_List.add(item: "brocolli", category: "vegetables")
@@ -11,7 +12,6 @@ describe Grocery_List do
       Grocery_List.add(item: "toilet roll", category: "toiletries")
 
       grocery_list = Grocery_List.all?
-
       expect(grocery_list.length).to eq 4
       expect(grocery_list.first).to be_a Grocery_List
       expect(grocery_list.first.item).to eq 'carrots'
--- spec/setup_test_database.rb
+++
@@ -1,7 +0,0 @@
-require 'pg'
-
-p "Setting up test database..."
-
-def setup_test_database
-  connection = PG.connect(dbname: 'grocery_app_test')
-end
